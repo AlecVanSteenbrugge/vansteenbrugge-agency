d5d91b9847d3c2c36bb556cd9ddd19d6c028995e
AlecVanSteenbrugge <alec.vansteenbrugge@student.ap.be>
2022-11-12 13:15:43 +0100

    added layout.module.css for styling and finishing the usestaticquery hook

---
+++ src/pages/artists.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+const ArtistsPage = () => {
+  return (
+    <Layout pageTitle="Artists of Van Steenbrugge Agency">
+      <p>A list of artists will be displayed here.</p>
+    </Layout>
+  )
+}
+export default ArtistsPage
